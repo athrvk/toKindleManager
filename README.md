a1956a4c7979c86a6293e5d1fa7a6da5aee73d25
Atharva Kusumbia <45625606+athrvk@users.noreply.github.com>
2024-03-08 21:38:37 +0530

    Revert "add custom status badge"

--- README.md
+++ README.md
@@ -2,7 +2,4 @@
 
 [![to Kindle Manager](https://github.com/athrvk/toKindleManager/actions/workflows/health_check.yml/badge.svg?event=schedule)](https://github.com/athrvk/toKindleManager/actions/workflows/health_check.yml)
 
-[![toKindle Health Status](https://github.com/athrvk/toKindleManager/actions/artifacts/workflows/health_check/latest/status-badge.svg)](https://github.com/athrvk/toKindleManager/actions/artifacts/workflows/health_check/latest/status-badge.svg)
-
-
 A cron job to keep to kindle service available 
--- script.sh
+++ script.sh
@@ -12,31 +12,33 @@ hit_api() {
 
 restart_server() {
     curl -s -o /dev/null -w "%{http_code}" \
-        --request POST \
-        --url "$TO_KINDLE_RESTART_URL" \
-        --header 'accept: application/json' \
-        --header 'authorization: Bearer $API_TOKEN'
+         --request POST \
+         --url "$TO_KINDLE_RESTART_URL" \
+         --header 'accept: application/json' \
+         --header 'authorization: Bearer $API_TOKEN'
 }
 
 # Primary API call
 status_code=$(hit_api "$toKindle")
 
 if [ "$status_code" -eq 200 ]; then
-    # Primary API is healthy 
-    echo "SUCCESS"  
-else
-    # Primary API has issues; attempt restart
+    # Success!
+    echo "to kindle is up!"
+fi
+
+if [ "$status_code" -eq 503 ]; then
+    # Primary API failed, try backup with retries
     for (( j=1; j<=$MAX_RETRIES; j++ )); do
         backup_status_code=$(restart_server)
 
         if [ "$backup_status_code" -eq 200 ]; then
-            # Restart succeeded
-            echo "SUCCESS"  
-            break
+            # Backup succeeded
+            echo "restarting to kindle"
+            break 
         fi
 
         if [ "$j" -eq "$MAX_RETRIES" ]; then
-            echo "FAILURE" 
+            echo "all retries exhausted for restarting to kindle"
         fi
     done
 fi
